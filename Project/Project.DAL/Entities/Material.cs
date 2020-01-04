7ce6443805e9d7a778fe6db2c021f603ccdb5a98
artyoda <artyoda05@gmail.com>
2020-01-04 15:58:33 +0200

    DAL first iteration entities

---
+++ Project/Project.DAL/Entities/Material.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.DAL.Entities
+{
+    public class Material
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Info { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Path { get; set; }
+        public User Author { get; set; }
+        public ICollection<Rating> Ratings { get; set; }
+    }
+}
---
+++ Project/Project.DAL/Entities/Rating.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.DAL.Entities
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public Material Material { get; set; }
+        public User Assigner { get; set; }
+    }
+}
